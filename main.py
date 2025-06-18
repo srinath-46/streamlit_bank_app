8efd21f4902c732c13e84d900e3b69f9f122dcfd
srinath <l.srinath160706@gmail.com>
2025-06-18 20:01:25 +0530

    main.py

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ if "user" not in st.session_state:
 
 # Login Function
 def login():
-    st.title("🏦  Thevudiya Bank ")
-    username = st.text_input("gopamavan ")
-    password = st.text_input("irumbu kol", type="password")
+    st.title("🏦 Streamlit Bank Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-    if st.button("oombu"):
+    if st.button("Login"):
         users_df = pd.read_csv("data/users.csv")
 
         # Validate columns
