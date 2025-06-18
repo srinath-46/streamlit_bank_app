982cc46f5e517a4fb66516b9f359080dc26cc1c5
srinath <l.srinath160706@gmail.com>
2025-06-19 00:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -329,7 +329,7 @@ def user_dashboard():
             save_csv(transactions_df, transactions_file)
             st.success(f"Payment of ₹{due_amount} via {payment_method} successful!")
 
-   if st.session_state.user:
+if st.session_state.user:
     st.sidebar.write(f"👋 Welcome, {st.session_state.user['username']}")
     if st.sidebar.button("Logout"):
         st.session_state.user = None
